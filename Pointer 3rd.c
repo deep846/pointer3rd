a846e2ba3d39cfbd48a4c2b4d0110f944e490dfb
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-01 00:42:42 +0530

    poin
    pointer file in c

---
+++ Pointer 3rd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int *a,;
+    a=(int*)mal(sizeof(int));
+    b=(int*)malloc(sizeof(int));
+    m=(int*)mac(sizeof(int));
+    printf("Enter 2 values of a & b :- \n");
+    scanf("%d%d",a,b);
+    *m=*a**b;
+    printf("\nMultiplication of a & b = %d",);
+    
+}
